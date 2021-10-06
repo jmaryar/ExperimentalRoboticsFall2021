3cd4ffe25240deb22547ecbd4c87c20623433790
jmaryar <59968747+jmaryar@users.noreply.github.com>
2021-10-06 12:29:43 -0400

    Update SMD_PWM_Motor_Controller.ino

--- SMD_PWM_Motor_Controller.ino
+++ SMD_PWM_Motor_Controller.ino
@@ -5,41 +5,64 @@ This code uses an Arduino uno to command one SMD PWM Motor COntroller (TE-294-00
 
 ***********************************************************/
 
-const int inAPin = 2;
-const int inBPin = 4;
-const int PWMPin = 3;
+const int inAPin_1 = 2;
+const int inBPin_1 = 4;
+const int PWMPin_1 = 3;
+
+const int inAPin_2 = 5;
+const int inBPin_2 = 7;
+const int PWMPin_2 = 6;
 
 void setup() {
 
-  pinMode(inAPin, OUTPUT);
-  pinMode(inBPin, OUTPUT);
-  pinMode(PWMPin, OUTPUT);
+  pinMode(inAPin_1, OUTPUT);
+  pinMode(inBPin_1, OUTPUT);
+  pinMode(PWMPin_1, OUTPUT);
+  
+  pinMode(inAPin_2, OUTPUT);
+  pinMode(inBPin_2, OUTPUT);
+  pinMode(PWMPin_2, OUTPUT);
 }
 
 void loop() {
   int i;
-  digitalWrite(inAPin, LOW); //stop motors can be both low or high
-  digitalWrite(inBPin, LOW);
+  digitalWrite(inAPin_1, LOW); //stop motors can be both low or high
+  digitalWrite(inBPin_1, LOW);
+  
+  digitalWrite(inAPin_2, LOW); //stop motors can be both low or high
+  digitalWrite(inBPin_2, LOW);
   
   delay(2000);
-  digitalWrite(inAPin, HIGH); // move one direction
-  digitalWrite(inBPin, LOW);
+  digitalWrite(inAPin_1, HIGH); // move one direction
+  digitalWrite(inBPin_1, LOW);
+  
+  digitalWrite(inAPin_2, HIGH); // move one direction
+  digitalWrite(inBPin_2, LOW);
   
   for (i = 0; i < 50; i++){ 
-     analogWrite(PWMPin, i);
+     analogWrite(PWMPin_1, i);
+     analogWrite(PWMPin_2, i);
      delay(10);
   }
    delay(2000);
 
-  digitalWrite(inAPin, LOW); // stop motor
-  digitalWrite(inBPin, LOW);
+  digitalWrite(inAPin_1, LOW); // stop motor
+  digitalWrite(inBPin_1, LOW);
+  
+  digitalWrite(inAPin_2, LOW); // stop motor
+  digitalWrite(inBPin_2, LOW);
+  
   delay(2000);
   
-  digitalWrite(inAPin, LOW); // move other direction
-  digitalWrite(inBPin, HIGH);
+  digitalWrite(inAPin_1, LOW); // move other direction
+  digitalWrite(inBPin_1, HIGH);
+  
+  digitalWrite(inAPin_2, LOW); // move other direction
+  digitalWrite(inBPin_2, HIGH);
   
   for (i = 0; i < 50; i++){
-     analogWrite(PWMPin,i);
+     analogWrite(PWMPin_1,i);
+     analogWrite(PWMPin_2,i);
      delay(10);
   }
    delay(2000);
