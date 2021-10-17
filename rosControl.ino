83be5e0dd4c537f6e40b7b52ed2146d9a504dbfa
jmaryar <59968747+jmaryar@users.noreply.github.com>
2021-10-17 18:08:46 -0400

    Update rosControl.ino

--- rosControl.ino
+++ rosControl.ino
@@ -4,6 +4,8 @@
 #include <geometry_msgs/Twist.h>
 #include "driveControl.h"
 
+//#include <package_name/TutorialsConfig.h> NOTE! CHANGE string 'Tutorials' to whatever you use in dynamic reconfigure cfg file"
+
 #define LEFTPWM 3
 #define LEFTA 2
 #define LEFTB 4
@@ -12,10 +14,18 @@
 #define RIGHTA 5
 #define RIGHTB 7
 
+#define LEFT_SERVO_PWM_PIN 9
+#define RIGHT_SERVO_PWM_PIN 10
+#define LEFT_SERVO_POS 90
+#define RIGHT_SERVO_POS 90
+
+
 ros::NodeHandle nh;
 
 Controller Left;
 Controller Right;
+
+Gripper Gripper;
 
 DriveControl Control;
 
@@ -27,16 +37,32 @@ void rightCallback(geometry_msgs::Twist& cmd_vel){
     Right.Drive(cmd_vel.linear.x, 1);
 }
 
+void dynamicCallback(package_name::TutorialsConfig &config uint32_t level){
+    if(config.IsOpen){
+        Gripper.Open();
+    }else{
+        Gripper.Close();
+    }
+}
+
 ros::Subscriber<geometry_msgs::Twist> leftSub("leftDrive", leftCallback);
 ros::Subscriber<geometry_msgs::Twist> rightSub("rightDrive", rightCallback);
+
+dynamic_reconfigure::Server<package_name::TutorialConfig> server;
+dynamic_reconfigure::Server<dynamic_tutorials::TutorialsConfig>::CallbackType f;
 
 void setup() {
     nh.initNode();
     nh.subscribe(leftSub);
     nh.subscribe(rightSub);
-
+    
+    
+    Gripper.init(LEFT_SERVO_PWM_PIN, RIGHT_SERVO_PWM_PIN, LEFT_SERVO_POS, RIGHT_SERVO_POS);
     Left.init(LEFTPWM, LEFTA, LEFTB);
     Right.init(RIGHTPWM, RIGHTA, RIGHTB);
+    
+    f= boost::bind(&dynamicCallback, _1,_2);
+    server.setCallback(f);
 
     Control.setControllers(Left, Right);
 }
